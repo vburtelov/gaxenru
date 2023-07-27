815e4d116e41f5e84e38582c78f1c7207a7aea33
Vadim Burtelov <v@burtelov.ru>
2023-07-27 23:36:36 +0300

    added pages elements

---
+++ app/tfp/page.jsx
@@ -0,0 +1,40 @@
+/*
+ * @author Vadim Burtelov https://burtelov.ru/
+ */
+
+import * as React from "react";
+import Typography from "@mui/material/Typography";
+import {Stack} from "@mui/material";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+export default function Tfp() {
+    return (
+        <Box sx={{flexGrow: 1}}>
+            <Stack direction="column">
+                <Box style={{
+                    backgroundImage: `url('VLOV6818.jpg')`,
+                    backgroundSize: "cover",
+                    width: "100vw",
+                    height: "100vh",
+                }}>
+                    <Stack
+                        direction="column"
+                        display="flex"
+                        justifyContent="flex-end"
+                        alignItems="flex-start"
+                        height="inherit"
+                    >
+                        <Container maxWidth="disable">
+                            <Typography variant="h1">
+                                A VISUAL
+                            </Typography>
+                            <Typography variant="h1" fontStyle="italic">
+                                EXPLORER
+                            </Typography>
+                        </Container>
+                    </Stack>
+                </Box>
+            </Stack>
+        </Box>
+    );
+}
